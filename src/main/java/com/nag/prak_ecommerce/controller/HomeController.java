3481123823f52a8253e2ae778d3af9cbba846d77
Prakash Pandey <prakash.pandey01@nagarro.com>
2025-03-27 13:01:03 +0530

    cloudfront url is given in cross origin without slash

--- src/main/java/com/nag/prak_ecommerce/controller/HomeController.java
+++ src/main/java/com/nag/prak_ecommerce/controller/HomeController.java
@@ -12,7 +12,7 @@ import java.util.Map;
 import java.io.IOException;
 //@CrossOrigin(origins = "http://localhost:4200")
 //@CrossOrigin(origins = "http://front-end-s3.s3-website.ap-south-1.amazonaws.com")
-@CrossOrigin(origins ="https://d7tzaevtczo3x.cloudfront.net/")
+@CrossOrigin(origins ="https://d7tzaevtczo3x.cloudfront.net")
 @RestController
 public class HomeController {
 
